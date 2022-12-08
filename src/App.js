6fd52f7e23bba6fd09c0f08cf60527ecb3cc42b0
hieufix1710 <hieufix1710@gmail.com>
2022-12-08 14:11:00 +0700

    remove unnesessary features

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ function App() {
     <MainLayout>
       <div
         style={{
-          backgroundImage: "url(wall_poro.jpeg)",
+          backgroundImage: "url(/wall_poro.jpeg)",
           height: 400,
           marginTop: 16,
           display: "flex",
           flexDirection: "column",
           justifyContent: "center",
           backgroundSize: "center",
-          backgroundPosition: 'center',
+          backgroundPosition: "center",
           objectFit: "cover",
         }}
       >
@@ -20,18 +20,15 @@ function App() {
           style={{
             color: "white",
             marginLeft: 20,
-
           }}
-        >
-          
-        </div>
+        ></div>
       </div>
       <div style={{ padding: "16px 0" }}>
         <div className="cards">
-          {[1,2,3,4,6,5].map((e, i) => {
+          {[1, 2, 3, 4, 6, 5].map((e, i) => {
             return (
               <div className="card" key={i}>
-                <img src="poro.jpeg" alt="image_alt" className="card_image" />
+                <img src="/poro.jpeg" alt="image_alt" className="card_image" />
                 <div className="btn">Touch</div>
               </div>
             );
